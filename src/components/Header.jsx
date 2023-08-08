dd5910407de9505ba545822cff073dc18c81dc3d
Luis <fasterplop@gmail.com>
2023-08-08 18:02:23 -0400

    observance instead of observanse

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className=" flex flex-row items-center text-center justify-center h-auto z-50 overflow-hidden">
       <Image src={header} alt="Logo" height={200} priority quality={100} />
       <h1 className="text-[#A16939] hidden sm:block ml-16 text-[36px] font-AlfaFont font-semibold">
-        AUGUSTINIAN MONKS OF THE PRIMITIVE OBSERVANSE
+        AUGUSTINIAN MONKS OF THE PRIMITIVE OBSERVANCE
       </h1>
     </div>
   );
